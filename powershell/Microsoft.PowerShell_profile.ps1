075346bdf00940fefbc88245c208b7b28917d048
Mateusz Loskot <mateusz@loskot.net>
2012-05-20 01:17:10 +0100

    Removed unused modules. Tidy up profile.ps1

--- powershell/Microsoft.PowerShell_profile.ps1
+++ powershell/Microsoft.PowerShell_profile.ps1
@@ -2,6 +2,21 @@
 
 $scripts = split-path $profile
 
+# Helper functions for user/computer session management
+function invoke-userLogout { shutdown /l /t 0 }
+function invoke-systemShutdown { shutdown /s /t 5 }
+function invoke-systemReboot { shutdown /r /t 5 }
+function invoke-systemSleep { RunDll32.exe PowrProf.dll,SetSuspendState }
+function invoke-terminalLock { RunDll32.exe User32.dll,LockWorkStation }
+
+# Aliases
+set-alias logout invoke-userLogout
+set-alias halt invoke-systemShutdown
+set-alias restart invoke-systemReboot
+if (test-path alias:\sleep) { remove-item alias:\sleep -force }
+set-alias sleep invoke-systemSleep -force
+set-alias lock invoke-terminalLock
+
 # Find the ssh-agent.exe executable
 # http://haacked.com/archive/2011/12/19/get-git-for-windows.aspx
 $env:path += ";" + (Get-Item "Env:ProgramFiles(x86)").Value + "\Git\bin"
