23f7788e808e5810f25b18f5da25d6459020f444
Mateusz Loskot <mateusz@loskot.net>
2012-05-22 22:53:36 +0100

    Added custom profile for posh-git - no more use of posh-git/profile.example.ps1

---
+++ powershell/posh-git_profile.ps1
@@ -0,0 +1,40 @@
+Push-Location (Split-Path -Path $MyInvocation.MyCommand.Definition -Parent)
+
+# Load posh-git module from current directory
+Import-Module .\modules\posh-git
+
+# If module is installed in a default location ($env:PSModulePath),
+# use this instead (see about_Modules for more information):
+# Import-Module posh-git
+
+
+# Set up a simple prompt, adding the git prompt parts inside git repos
+function prompt {
+    $realLASTEXITCODE = $LASTEXITCODE
+
+    # Reset color, which can be messed up by Enable-GitColors
+    $Host.UI.RawUI.ForegroundColor = $GitPromptSettings.DefaultForegroundColor
+
+    Write-Host($pwd) -nonewline
+
+    Write-VcsStatus
+
+    $global:LASTEXITCODE = $realLASTEXITCODE
+    return "> "
+}
+
+Enable-GitColors
+
+Pop-Location
+
+# Find and launch pageant.exe, load secret key
+$ppk = join-path ${env:UserProfile} ".ssh\mloskot@dog.putty.ppk"
+if (test-path $ppk)
+{
+    $pageant = Get-ProgramFilesPathX86("PuTTY\pageant.exe")
+    if (test-path $pageant)
+    {
+        Write-Verbose("Loading '$ppk' with '$pageant'")
+        & $pageant $ppk
+    }
+}
